b863006921128fe5d4b989e4bf9efc3bef4e34ec
jack <1475813695@qq.com>
2022-03-07 16:02:48 +0800

    方法引用

--- src/java/com/roc/practice/lambda/FunctionTest.java
+++ src/java/com/roc/practice/lambda/FunctionTest.java
@@ -7,10 +7,13 @@ import java.util.function.Supplier;
 
 public class FunctionTest {
     public static void main(String[] args) {
+        //断言
         Predicate<Integer> predicate = (i)-> i>0;
         System.out.println(predicate.test(1));
+        //消费者
         Consumer<Integer> consumer = (i) -> System.out.println(i);
         consumer.accept(2);
+        //提供一个数据
         Supplier<Integer> supplier = ()-> 1;
     }
 
