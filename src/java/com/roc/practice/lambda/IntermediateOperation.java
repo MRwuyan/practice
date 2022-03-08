9620ea9940f3322b5df514ead6510503d052fe1d
jack <1475813695@qq.com>
2022-03-08 13:37:59 +0800

    stream 写法2

---
+++ src/java/com/roc/practice/lambda/IntermediateOperation.java
@@ -0,0 +1,13 @@
+package com.roc.practice.lambda;
+
+/**
+ * 中间操作
+ */
+public class IntermediateOperation {
+    public static void main(String[] args) {
+        //有状态 map，mapToxxx，flatMap。。
+
+
+        //无状态 distinct sorted limit
+    }
+}
