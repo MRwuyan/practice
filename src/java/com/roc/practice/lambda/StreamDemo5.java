b66e1f9b6b3a4f689715da5d2ff5767e9997e173
jack <1475813695@qq.com>
2022-03-09 11:48:26 +0800

    并行流

--- src/java/com/roc/practice/lambda/StreamDemo5.java
+++ src/java/com/roc/practice/lambda/StreamDemo5.java
@@ -1,13 +1,41 @@
 package com.roc.practice.lambda;
 
-import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
 
+/**
+ * 并行流
+ */
 public class StreamDemo5 {
-    private static int i=0;
-    public static void main(String[] args) {
-        new Random().ints().parallel().forEach(s-> StreamDemo5.add());
+    private static int i = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+//        new Random().ints().parallel().forEach(s -> StreamDemo5.add());
+        //当有多个流声明时，取最后一次
+//        long count = IntStream.range(1, 100).parallel().peek(s -> StreamDemo5.add()).sequential().peek(a -> StreamDemo5.add1()).limit(10).count();
+        //并行流使用的线程ForkJoinPool.commonPool，（当前机器的cpu个数）
+//        long count = IntStream.range(1, 100).parallel().peek(s -> StreamDemo5.add()).count();
+//        java.util.concurrent.ForkJoinPool
+//        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "20");
+//        IntStream.range(1, 100).parallel().peek(a -> StreamDemo5.add()).count();
+        ForkJoinPool pool = new ForkJoinPool(20);
+        pool.submit(() -> IntStream.range(1, 100).parallel().peek(a -> StreamDemo5.add()).count());
+        pool.shutdown();
+        Thread.sleep(10000L);
     }
-    public static int add(){
+
+    public static int add() {
+        i++;
+        System.err.println(Thread.currentThread().getName() + "-->" + i);
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return i;
+    }
+
+    public static int add1() {
         i++;
         System.out.println(i);
         try {
