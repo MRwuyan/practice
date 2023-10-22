b4ff1d6c0a24910b112f8076ea374be82d89adc2
pore <1475813695@qq.com>
2023-10-22 21:09:15 +0800

    添加python

--- pythonDemo/src/HelloWord.py
+++ pythonDemo/src/HelloWord.py
@@ -9,3 +9,34 @@ print(True,False)
 print(3 > 2)
 
 
+print('\\\t\\')
+
+print(r'\\\t\\')
+print('''line1
+... line2
+... line3''')
+
+print(10/3)
+print(ord('A'))
+print(ord('中'))
+x = b'ABC'
+print('ABC'.encode('ascii'))
+print(len('ABC'))
+print( len('中文'))
+print(len(b'ABC'))
+
+print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'))
+
+# ，%运算符就是用来格式化字符串的。在字符串内部，%s表示用字符串替换，%d表示用整数替换，有几个%?占位符，后面就跟几个变量或者值，顺序要对应好。如果只有一个%?，括号可以省略。
+print('Hello, %s' % 'world')
+print('Hi, %s, you have $%d.' % ('Michael', 1000000))
+print('%0004d-%02d' % (3, 1))
+print('%.3f' % 3.1415926)
+# 有些时候，字符串里面的%是一个普通字符怎么办？这个时候就需要转义，用%%来表示一个%：
+print( 'growth rate: %d %%' % 7)
+print('Hello, {0}, 成绩提升了 {1:.1f}%'.format('小明', 17.125))
+
+r = 2.5
+s = 3.14 * r ** 2
+print( r ** 2)
+print(f'The area of a circle with radius {r} is {s:.2f}')
---
+++ pythonDemo/src/listDemo.py
@@ -0,0 +1,16 @@
+classmates = ['Michael', 'Bob', 'Tracy']
+classmates.append('Adam')
+classmates.pop(1)
+classmates.insert(5, 'Jack')
+print(classmates)
+
+sortList=(1,2,3,4,5,6,7,8,9)
+print(sortList[0:3])
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0])
+print(L[1][1])
